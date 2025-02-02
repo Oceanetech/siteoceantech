79ada198b13014c1e276bd924984db5d5676664e
paulorocha03 <pgrocha03@gmail.com>
2025-02-01 21:54:05 -0300

    Funcionalidade de envio de e-mail inclusa - Necessita reconfigurar para e-mail da empresa e configurar SMTP, testes internos funcionais.

---
+++ iLanding-1.0.0/assets/js/sendemail.js
@@ -0,0 +1,45 @@
+//COnfiguração do envio do email
+const nodemailer = require("nodemailer")
+
+
+//Função para configurar o email que enviará o e-mail do cliente
+const sendEmail = async (name, email, subject, message) => {
+    //Transporter para envio do e-mail
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS
+        }
+      });
+    // função para verificar acesso ao email
+    // Verificar a conexão
+    transporter.verify((error, success) => {
+        if (error) {
+        console.error('Erro de conexão com o servidor SMTP:', error);
+        } else {
+        console.log('Servidor SMTP está pronto para enviar e-mails!');
+        }
+    });
+
+//função de enviar o email
+
+    let mailOptions = {
+        from: process.env.EMAIL_USER, // email que envia
+        to: 'pgrocha03@gmail.com',
+        subject: subject,
+        text: `Mensagem de ${name} (${email}):\n\n${message}`
+    }
+
+    try {
+        //Tentando enviar e-mail
+        await transporter.sendMail(mailOptions)
+        return {success: true, message: "E-mail enviado com sucesso, verificar caixa de entrada e spam"}
+    } catch (error) {
+        return {sucess: false, message: `Erro ao enviar o e-mail: ${error.message}`}
+    }
+
+}
+
+module.exports = sendEmail
